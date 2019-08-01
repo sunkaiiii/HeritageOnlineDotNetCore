7c5b1f46cb566ac12d80449b89650a382dcbc55b
孙楷 <704725181@qq.com>
2019-08-01 16:48:32 +1000

    添加依赖

---
+++ HeritageWebserviceDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeritageWebserviceDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

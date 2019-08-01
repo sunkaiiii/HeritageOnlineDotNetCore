8756f4efa21364baea8cb807f6a383609533f215
孙楷 <704725181@qq.com>
2019-08-01 17:07:53 +1000

    一些Mongodb的实例

--- HeritageWebserviceDotNetCore/Program.cs
+++ HeritageWebserviceDotNetCore/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Linq;
 
 namespace HeritageWebserviceDotNetCore
 {
@@ -6,7 +9,30 @@ namespace HeritageWebserviceDotNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("heritage");
+            var collection = database.GetCollection<BsonDocument>("heritage");
+            var document = new BsonDocument
+            {
+                {"name","test2" },
+                {"type","haha" },
+                {"info",new BsonDocument
+                {
+                    {"x",203 },
+                    {"y",102 }
+                } }
+            };
+            collection.InsertOne(document);
+
+            var documents = Enumerable.Range(0, 100).Select(i => new BsonDocument("counter", i));
+            collection.InsertMany(documents);
+            var count = collection.CountDocuments(new BsonDocument());
+            Console.WriteLine(count);
+            var document2 = collection.Find(new BsonDocument()).FirstOrDefault();
+            Console.WriteLine(document2);
+            var documents2 = collection.Find(new BsonDocument()).ToList();
+            documents2.ForEach(doc => Console.WriteLine(doc));
+
         }
     }
 }

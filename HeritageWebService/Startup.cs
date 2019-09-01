23a80eccbe05a4ddf897979dcb9d3912642c302c
孙楷 <704725181@qq.com>
2019-09-01 14:55:06 +1000

    新闻列表页请求
    1、新闻列表页接口

--- HeritageWebService/Startup.cs
+++ HeritageWebService/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
@@ -34,10 +35,13 @@ namespace HeritageWebService
         {
             services.Configure<HeritageMainPageListSettings>(Configuration.GetSection(nameof(HeritageMainPageListSettings))); //将appsetings.json当中的属性进行依赖填充
             services.Configure<HeritageNewsDetailSettings>(Configuration.GetSection(nameof(HeritageNewsDetailSettings)));
+            services.Configure<HeritageNewsListSettings>(Configuration.GetSection(nameof(HeritageNewsListSettings)));
             services.AddSingleton<IHeritageMainPageListSettings>(sp => sp.GetRequiredService<IOptions<HeritageMainPageListSettings>>().Value); //接口的单一实例以单例在服务生存期DI中注册
             services.AddSingleton<HeritageService>(); //向DI注册了HeritageService的类，以支持消费类中的构造函数注入。单例存在于整个服务周期是最合适的。 根据官方 Mongo Client 重用准则，应使用单一实例服务生存期在 DI 中注册 MongoClient。
             services.AddSingleton<IHeritageNewsDetailSettings>(sp => sp.GetRequiredService<IOptions<HeritageNewsDetailSettings>>().Value);
             services.AddSingleton<NewsDetailService>();
+            services.AddSingleton<IHeritageNewsListSettings>(sp => sp.GetRequiredService<IOptions<HeritageNewsListSettings>>().Value);
+            services.AddSingleton<NewsListService>();
 
             //将Swagger生成器添加到方法中的服务集合中
             services.AddSwaggerGen(c =>
@@ -74,6 +78,11 @@ namespace HeritageWebService
             }
 
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(@"C:\Users\sunkai\Documents\GitHub\HeritageWebserviceDotNetCore\HeritageWebService\img"),
+                RequestPath = string.Empty
+            }) ;
             //允许中间件为生成的JSON文档和Swagger UI提供服务
             app.UseSwagger();
 

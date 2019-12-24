e2ef86e3e2692a25bfdde021d442d262f53c7b49
孙楷 <704725181@qq.com>
2019-12-24 21:52:03 +0800

    添加图片压缩
    1、添加图片压缩方法

---
+++ HeritageWebserviceDotNetCore.Tests/WebImageSaverTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using HeritageWebserviceDotNetCore.Reptile;
+using NUnit.Framework;
+
+namespace HeritageWebserviceRetileDotNetCore.Tests
+{
+    public class WebImageSaverTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestGetCompressImageName()
+        {
+            var imagePath = "/Users/sunkai/Documents/GitHub/HeritageOnlineDotNetCore/HeritageWebserviceDotNetCore/img/420715940a814eaebe774e478bc4b662.jpg";
+            var exptectedResult = "/Users/sunkai/Documents/GitHub/HeritageOnlineDotNetCore/HeritageWebserviceDotNetCore/img/420715940a814eaebe774e478bc4b662_compress.jpg";
+            Assert.AreEqual(exptectedResult, WebImageSaver.Instance.GetComressImageName(imagePath));
+        }
+
+
+        [Test]
+        public void TestImageCompress()
+        {
+            var imagePath = "/Users/sunkai/Documents/GitHub/HeritageOnlineDotNetCore/HeritageWebserviceDotNetCore/img/DSC01789.JPG";
+            WebImageSaver.Instance.CompressImage(imagePath);
+            Assert.IsTrue(File.Exists(WebImageSaver.Instance.GetComressImageName(imagePath)));
+        }
+
+
+    }
+}
--- HeritageWebserviceDotNetCore/Reptile/WebImageSaver.cs
+++ HeritageWebserviceDotNetCore/Reptile/WebImageSaver.cs
@@ -4,16 +4,21 @@ using System.Net;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using HeritageWebserviceReptileDotNetCore.Reptile;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace HeritageWebserviceDotNetCore.Reptile
 {
-    class WebImageSaver
+    public class WebImageSaver
     {
         private static readonly Lazy<WebImageSaver> lazy = new Lazy<WebImageSaver>(() => new WebImageSaver());
         public static WebImageSaver Instance { get { return lazy.Value; } }
 
         public BufferBlock<string> ImageTargetBlock { get; }
-        public WebImageSaver()
+        private WebImageSaver()
         {
             ImageTargetBlock = new BufferBlock<string>();
         }
@@ -41,6 +46,7 @@ namespace HeritageWebserviceDotNetCore.Reptile
                 {
                     Console.WriteLine("Starting to save image {0}", imageUrl);
                     wc.DownloadFile(imageUrl, savePath);
+                    CompressImage(savePath);
                     Console.WriteLine("Save image {0} completely", imageUrl);
                 }
                 catch(WebException e)
@@ -54,6 +60,43 @@ namespace HeritageWebserviceDotNetCore.Reptile
                 }
             }
             return 1;
+        }
+
+        //给原始图片做一次压缩
+        //并且生成一个小图
+        public void CompressImage(string savePath)
+        {
+            const int boundWidth = 480;
+            const int boundHeight = 320;
+            using (Image image = Image.Load(savePath))
+            {
+                Console.WriteLine(image.GetType().Name);
+                int resizeWidth=image.Width;
+                int resizeHeight=image.Height;
+                if(resizeHeight > boundHeight)
+                {
+                    double scale = (double)boundHeight / image.Height;
+                    resizeHeight = (int)(resizeHeight * scale);
+                    resizeWidth = (int)(resizeWidth * scale);        
+                }
+                if(resizeWidth>boundWidth)
+                {
+                    double scale = (double)boundWidth / image.Width;
+                    resizeWidth = (int)(resizeWidth * scale);
+                    resizeHeight = (int)(resizeHeight * scale);
+                }
+                Console.WriteLine("Height:"+resizeHeight + " Width:" + resizeWidth);
+                image.Mutate(x => x.Resize(resizeWidth, resizeHeight));
+                var saveCompressName = GetComressImageName(savePath);
+                var extensionName = Path.GetExtension(saveCompressName);
+                image.Save(saveCompressName);
+                
+            }
+        }
+
+        public string GetComressImageName(string originalSavePath)
+        {
+            return Path.Combine(Path.GetDirectoryName(originalSavePath),Path.GetFileNameWithoutExtension(originalSavePath)) + "_compress" + Path.GetExtension(originalSavePath);
         }
     }
 }
